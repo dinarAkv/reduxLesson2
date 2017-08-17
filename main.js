064cf720a5a55c034856c6474fa9ef0b09066818
Di <d.akhmetzyanov.stc@innopolis.ru>
2017-08-17 23:23:41 +0300

    Add listener to button.

--- main.js
+++ main.js
@@ -24,3 +24,10 @@ store.subscribe(() => {
 
 store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' });
 store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
+
+const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+
+addTrackBtn.addEventListener('click', () => {
+  const trackName = document.querySelectorAll('.trackInput')[0].value;
+  console.log('trackName', trackName);
+})
